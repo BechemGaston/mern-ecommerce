ea3854ae65afb7d9575840ef9b76845d94f45420
bechem gaston <bechemgaston07@gmail.com>
2022-06-20 02:36:08 +0800

    products

---
+++ client/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: "Zara Summer Shirt",
+            slug: "zara-summer-shirt",
+            category: "Shirts",
+            image: "/images/zara1.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: "Zara",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirt",
+
+        },
+        {
+            name: " Zara wide Legs",
+            slug: "zara-wide-legs",
+            category: "Pants",
+            image: "/images/pants2.jpg",
+            price: 180,
+            countInStock: 20,
+            brand: "Zara",
+            rating: 4,
+            numReviews: 10,
+            description: "high quality pants",
+
+        },
+        {
+            name: "Men Gray Pants",
+            slug: "men-gray-pants",
+            category: "Pants",
+            image: "/images/pants1.jpg",
+            price: 250,
+            countInStock: 10,
+            brand: "Zara",
+            rating: 4,
+            numReviews: 20,
+            description: "high quality pants",
+
+        },
+        {
+            name: "Women wide legs",
+            slug: "womrn-wide-legs",
+            category: "Pants",
+            image: "/images/wide2.jpg",
+            price: 210,
+            countInStock: 10,
+            brand: "Zara",
+            rating: 4.5,
+            numReviews: 20,
+            description: "high quality pants",
+
+        },
+    ]
+}
+
+export default data;
